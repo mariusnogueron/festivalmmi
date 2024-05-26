d0e3e289d0449ce36dd5224db97603852b01f158
mariusnogueron <marius.nogueron@gmail.com>
2024-05-26 14:41:37 +0200

    sitefinal

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Lenis from 'lenis'
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-
+import { ScrollToPlugin } from "gsap/ScrollToPlugin"; 
 
 
 var loader = document.getElementById("loader");
@@ -27,78 +26,22 @@ gsap.ticker.lagSmoothing(0)
 
 
 
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
-function fermerMenu() {
-    console.log("Fermeture du menu");
-    document.getElementById("menu").classList.add("hidden");
-}
-
-// Récupérer tous les liens dans le menu et leur ajouter un écouteur d'événements de clic
-document.querySelectorAll("#menu a").forEach(item => {
-    item.addEventListener("click", function() {
-        console.log("Clic sur le lien dans le menu");
-        fermerMenu();
-    });
-});
 
 
 
 
-
-
-
- 
-
-
-
-
-
-
-
-
-
-// ----------------------------------------------------------
-
-const buttonMenu = document.getElementById("buttonMenu")
-const menu = document.getElementById("menu")
-const menuList = document.getElementById("menuList")
-const home = document.getElementById("home")
-
-let menuVisible = false;
-
-buttonMenu.addEventListener("click", () => {
-    if (menuVisible) {
-        menu.style.display = "none"
-        buttonMenu.classList.add("text-beige");
-        buttonMenu.classList.remove("text-orange");
-        buttonMenu.classList.add("bg-orange");
-        buttonMenu.classList.remove("bg-beige");
-        home.classList.add("text-orange")
-        home.classList.remove("text-beige")
-        
-    }
-    else {
-        menu.style.display = "flex";
-        buttonMenu.classList.remove("text-beige");
-        buttonMenu.classList.add("text-orange");
-        buttonMenu.classList.remove("bg-orange");
-        buttonMenu.classList.add("bg-beige");
-        home.classList.add("text-beige");
-        home.classList.remove("text-orange");
-    }
-    menuVisible = !menuVisible;
-})
 
 // -----------------------------------------------
 
 const nav = document.getElementById("navBar")
+
 
 nav.style.display = "none"
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 700) {
         nav.style.display = "flex"
+
         
     } if (window.scrollY > 800) {
         nav.style.top = 0
@@ -129,7 +72,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 const project = document.querySelector(".project-container");
 
-// Vérifie si l'élément project-container est trouvé
 
 function getScrollAmount() {
     const screenWidth = window.innerWidth;
@@ -143,15 +85,12 @@ function getScrollAmount() {
     }
 }
 
-// Vérifie si la fonction getScrollAmount retourne une valeur correcte
 const scrollAmount = getScrollAmount();
 
 if (scrollAmount === 0) {
 }
 
-// Ajuster la durée de l'animation pour inclure une marge au début et à la fin
-const startDelay = 50; // px à attendre avant de commencer le défilement
-const endDelay = 50; // px à attendre après le défilement
+const endDelay = 200; 
 
 const tween = gsap.to(project, {
     x: getScrollAmount,
@@ -161,13 +100,12 @@ const tween = gsap.to(project, {
 
 ScrollTrigger.create({
     trigger: "#projects",
-    start: `top top`, // Ajouter un délai au début
-    end: () => `+=${project.scrollWidth - window.innerWidth + endDelay}`, // Ajouter un délai à la fin
+    start: `top top`, 
+    end: () => `+=${project.scrollWidth - window.innerWidth + endDelay}`, 
     pin: true,
     animation: tween,
     scrub: 1,
     invalidateOnRefresh: true,
-    markers: true,
 });
 
 
@@ -178,17 +116,94 @@ function fillSVGs() {
     const containerWidth = container.parentElement.getBoundingClientRect().width;
     const svgCount = Math.ceil(containerWidth / svgWidth);
 
-    // Clear existing SVGs
     container.innerHTML = '';
 
-    // Add the necessary number of SVGs to fill the container
     for (let i = 0; i < svgCount; i++) {
       const clonedSvg = svg.cloneNode(true);
       container.appendChild(clonedSvg);
     }
   }
 
-  // Fill the SVGs on initial load
   fillSVGs();
 
- 
+  document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const homeButton = document.getElementById("homeButton");
+
+    // Déclencheur pour changer la couleur du bouton en fonction de l'entrée et de la sortie du champ
+    ScrollTrigger.create({
+        trigger: "#projects",
+        start: "top top",
+        end: "+=4050px",
+        onEnter: () => {
+            homeButton.classList.remove("text-orange");
+            homeButton.classList.add("text-white");
+        },
+        onLeave: () => {
+            homeButton.classList.remove("text-white");
+            homeButton.classList.add("text-orange");
+        },
+        onEnterBack: () => {
+            homeButton.classList.remove("text-orange");
+            homeButton.classList.add("text-white");
+        },
+        onLeaveBack: () => {
+            homeButton.classList.remove("text-white");
+            homeButton.classList.add("text-orange");
+        },
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buttonMenu = document.getElementById("buttonMenu");
+    const menu = document.getElementById("menu");
+    const menuLinks = document.querySelectorAll("#menuList a");
+    
+    const homeButton = document.getElementById("homeButton");
+
+    let menuVisible = false;
+
+    function toggleMenu() {
+        if (!menuVisible) {
+            menu.style.display = "flex";
+            buttonMenu.classList.remove("text-beige");
+            buttonMenu.classList.add("text-orange");
+            buttonMenu.classList.remove("bg-orange");
+            buttonMenu.classList.add("bg-beige");
+            homeButton.classList.add("text-beige");
+            homeButton.classList.remove("text-orange");
+            menuVisible = true;
+        } else {
+            fermerMenu();
+        }
+    }
+
+    function fermerMenu() {
+        menu.style.display = "none";
+        buttonMenu.classList.add("text-beige");
+        buttonMenu.classList.remove("text-orange");
+        buttonMenu.classList.add("bg-orange");
+        buttonMenu.classList.remove("bg-beige");
+        homeButton.classList.add("text-orange");
+        homeButton.classList.remove("text-beige");
+        menuVisible = false;
+    }
+
+    buttonMenu.addEventListener("click", toggleMenu);
+
+    menuLinks.forEach(item => {
+        item.addEventListener("click", function() {
+            fermerMenu();
+        });
+    });
+
+    document.addEventListener("click", function(event) {
+        if (!menu.contains(event.target) && !buttonMenu.contains(event.target)) {
+            fermerMenu();
+        }
+    });
+});
+
